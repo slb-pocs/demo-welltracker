d010c7899e800590fd0b9082cf07e20ba3cbaa05
nilson <nilson.education@outlook.com>
2024-05-31 06:34:03 -0500

    popup view created

--- src/app/popup-view/popup-view.component.ts
+++ src/app/popup-view/popup-view.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-popup-view',
   templateUrl: './popup-view.component.html',
   styleUrl: './popup-view.component.css'
 })
-export class PopupViewComponent {
+export class PopupViewComponent implements OnInit {
+  message;
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any){
+    this.message=data.message;
+  }
 
+  ngOnInit(): void {    
+  }
 }
