ca7a3668c9d641cd32350b8dffc79f131bfd0f1b
nilson <nilson.education@outlook.com>
2024-05-31 06:28:48 -0500

    app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,36 @@ import { SurfaceEquipmentViewComponent } from './surface-equipment-view/surface-
 import { TrackrecordViewComponent } from './trackrecord-view/trackrecord-view.component';
 import { WellViewComponent } from './well-view/well-view.component';
 
+import{MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatStepperModule} from '@angular/material/stepper';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatDialogModule } from '@angular/material/dialog';
+import {
+  
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {  MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatTableModule} from '@angular/material/table';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +51,31 @@ import { WellViewComponent } from './well-view/well-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatAccordion,
+    MatExpansionModule,
+    MatDatepickerModule,
+    MatStepperModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+    MatTableModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatSlideToggleModule
   ],
   providers: [
     provideClientHydration(),
