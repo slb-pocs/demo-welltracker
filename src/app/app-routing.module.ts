8709b16c640ae70c8f281d5b03ab156376829381
nilson <nilson.education@outlook.com>
2024-05-31 06:47:57 -0500

    app routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter, withComponentInputBinding } from '@angular/router';
+import { WellViewComponent } from './well-view/well-view.component';
+import { TrackrecordViewComponent } from './trackrecord-view/trackrecord-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:WellViewComponent},
+  {path:'home', component:WellViewComponent},
+  {path:'create-well', component:WellViewComponent},
+  {path:'track-record/:id', component:TrackrecordViewComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[
+    provideRouter(routes,withComponentInputBinding())
+  ]
 })
 export class AppRoutingModule { }
